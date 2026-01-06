e013982e53fc6de5d4a922cc8d51995c4ebd10ce
ErfanYousefabadi <erfan.yoosefabadi@gmail.com>
2026-01-06 22:59:43 +0330

    implemented raycasting and DDA

--- Makefile
+++ Makefile
@@ -9,8 +9,8 @@ c: raycaster
 run: raycaster
 	./raycaster
 
-raycaster: main.c map.c player.c
-	gcc main.c map.c player.c -o raycaster $(CFLAGS)
+raycaster: main.c map.c player.c raycaster.c
+	gcc main.c map.c player.c raycaster.c -o raycaster $(CFLAGS)
 
 clean:
 	rm raycaster
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #define RAYLIB_PROJECT_ERFAN
+#define PLAYER_PROJECT_ERFAN
 #include "raylib.h"
 #include "map.h"
 #include "player.h"
+#include "raycaster.h"
 
 int main () {
     
@@ -13,10 +15,9 @@ int main () {
     
     int map_2d[N][N];
     Player p;
-    const float v = 70;
-    const int raycnt = 800;
-    p.pos.x = (N / 2 + .5) * TILE + pos_map.x;
-    p.pos.y = (N / 2 + .5) * TILE + pos_map.y;
+    const float v = 50;
+    p.pos.x = (N / 2.) * TILE + pos_map.x;
+    p.pos.y = (N / 2.) * TILE + pos_map.y;
     p.dir.x = 1, p.dir.y = 0;
     p.plane.x = 0, p.plane.y = 0.66;
     InitMap(N, map_2d);
@@ -28,9 +29,9 @@ int main () {
 
         // update player
         if (IsKeyDown(KEY_LEFT))
-            RotatePlayer(&p, -dt * PI);
+            RotatePlayer(&p, -dt * PI / 2);
         if (IsKeyDown(KEY_RIGHT))
-            RotatePlayer(&p, dt * PI);
+            RotatePlayer(&p, dt * PI / 2);
 
         if (IsKeyDown(KEY_W))
             MovePlayerForward(&p, v, dt, N, map_2d, TILE, pos_map);
@@ -43,14 +44,9 @@ int main () {
 
         BeginDrawing();
             ClearBackground((Color){0, 102, 204, 255});
-            DrawRectangle(0, WY / 2, WX, WY / 2, GRAY);
+            DrawRectangle(0, WY / 2, WX, WY / 2, DARKGREEN);
+            Raycast(p, N, map_2d, pos_map, TILE, WX, WY);
             DrawMap(pos_map, TILE, N, map_2d);
-            Vector2 DeltaRay = (Vector2){p.plane.x * 2 / raycnt, p.plane.y * 2 / raycnt};
-            for (int i = 0; i < raycnt; i++) {
-                Vector2 ray = {p.dir.x + p.plane.x - i * DeltaRay.x, p.dir.y + p.plane.y - i * DeltaRay.y};
-
-                DrawLine(p.pos.x, p.pos.y, p.pos.x + ray.x * 40, p.pos.y + ray.y * 40, PINK);
-            }
             ShowPlayer(p);
         EndDrawing();
     }
